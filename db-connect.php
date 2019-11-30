f7915e87598c2efa853e0e05c1770014b83f35b1
Tarun Joseph <tarunjoseph94@gmail.com>
2019-11-30 21:47:56 +0530

    Table to dump data

--- db-connect.php
+++ db-connect.php
@@ -1,8 +1,15 @@
 <?php
+//Cloud Database
 $servername = "remotemysql.com";
 $username = "w7d7KKX28F";
 $password = "WS9rHsSbKI";
 $dbname="w7d7KKX28F";
+//Localhost
+// $servername = "localhost";
+// $username = "root";
+// $password = "";
+// $dbname="iot";
+
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password,$dbname);
---
+++ index.php
@@ -0,0 +1,74 @@
+<?php
+  include 'db-connect.php';
+  $sql = "SELECT * FROM `log`;
+  $res2=mysqli_query($conn, $sql2);
+?>
+<table>
+  <tr>
+    <th>
+      Sl no
+    </th>
+    <th>
+      Humidity
+    </th>
+    <th>
+      Temp
+    </th>
+    <th>
+      Moist
+    </th>
+    <th>
+      Light
+    </th>
+    <th>
+      Date
+    </th>
+    <th>
+      Time
+    </th>
+  </tr>
+
+<?php
+  while ($row = $res2->fetch_assoc()){
+?>
+  <tr>
+  <td>
+  <?php
+    echo $row['Sl_pk'];
+    ?>
+  </td>
+    <td>
+    <?php
+      echo $row['humid'];
+      ?>
+    </td>
+    <td>
+    <?php
+      echo $row['temp'];
+      ?>
+    </td>
+    <td>
+    <?php
+      echo $row['moist'];
+      ?>
+    </td>
+    <td>
+    <?php
+      echo $row['light'];
+      ?>
+    </td>
+    <td>
+    <?php
+      echo $row['Date'];
+      ?>
+    </td>
+    <td>
+    <?php
+      echo $row['Time'];
+      ?>
+    </td>
+  </tr>
+$batch=$row['batch'];
+}
+</table>
+?>
