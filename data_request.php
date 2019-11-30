4a97d48ced9fad13cdcebcbe9d93f8a7d062431a
Tarun Joseph <tarunjoseph94@gmail.com>
2019-11-30 20:37:35 +0530

    First

---
+++ data_request.php
@@ -0,0 +1,27 @@
+<?php
+include 'db-connect.php';
+  date_default_timezone_set('Asia/Kolkata');
+   $d = date("Y-m-d");
+   //echo " Date:".$d."<BR>";
+   $t = date("H:i:s");
+
+   if(!empty($_GET['humid']) && !empty($_GET['temp']) && !empty($_GET['moisture']) && !empty($_GET['light'])) {
+     $humid = $_GET['humid'];
+     $temp = $_GET['temp'];
+     $moist = $_GET['moisture'];
+     $light = $_GET['light'];
+
+     $sql = "INSERT INTO Log (humid, temp,moist,light, Date, Time) VALUES ('".$humid."', '".$temp."','".$moist."', '".$light."', '".$d."', '".$t."')";
+
+   if (mysqli_query($conn,$sql)) {
+       echo "OK";
+   } else {
+       echo "Error: " . $sql . "<br>" . $conn->error;
+   }
+
+
+
+ $conn->close();
+}
+
+?>
